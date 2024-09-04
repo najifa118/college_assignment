6bdfd093576782a9c212b38c17c0e801da8c4ea9
Najifa Parvin <najifaparvin36@gmail.com>
2024-09-04 23:35:02 +0530

    Delete Java/Week1 directory

--- Java/Week1/copyright.java
+++
@@ -1,7 +0,0 @@
-public class copyright {
-    public static void main(String[] args) {
-        System.out.println("Copyright © 2024 Najifa parvin. All rights reserved.\n \n");
-        
-    }
-
-}
--- Java/Week1/myName.java
+++
@@ -1,6 +0,0 @@
-public class myName {
-    public static void main(String[] args) {
-        copyright.main(args);
-        System.out.println("My name is Subha Saha");
-    }
-}
