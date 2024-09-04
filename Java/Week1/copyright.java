4faac70c52de9510dd30836fe516802463a4b192
Najifa Parvin <najifaparvin36@gmail.com>
2024-09-04 23:32:26 +0530

    Add files via upload

---
+++ Java/Week1/copyright.java
@@ -0,0 +1,7 @@
+public class copyright {
+    public static void main(String[] args) {
+        System.out.println("Copyright © 2024 Najifa parvin. All rights reserved.\n \n");
+        
+    }
+
+}
---
+++ Java/Week1/myName.java
@@ -0,0 +1,6 @@
+public class myName {
+    public static void main(String[] args) {
+        copyright.main(args);
+        System.out.println("My name is Subha Saha");
+    }
+}
